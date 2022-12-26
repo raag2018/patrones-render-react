e0211f196d903cf5d9fe014e8544e72475136a28
Roberto Antonio Alferes Gomez <roberto3652017@gmail.com>
2022-12-26 11:21:29 -0600

    explicacion de la composicion de componentes y colocacion del estado en react

--- src/App/index.js
+++ src/App/index.js
@@ -1,8 +1,12 @@
 //import logo from './logo.svg';
 import React from 'react';
-import { TodoProvider } from '../TodoContext';
+/*import { TodoProvider } from '../TodoContext';
 import { AppUI } from './AppUI';
 import './App.css';
+import { TodoList } from '../TodoList/TodoList';
+import { TodoCounter } from '../TodoCounter/TodoCounter';
+import { TodoSearch } from '../TodoSearch/TodoSearch';
+import { TodoItem } from '../TodoItem/TodoItem';
 /*const defaultTodos = [
 	{text: "Comida 1", complete: false},
 	{text: "Comida 2", complete: true},
@@ -19,7 +23,49 @@ import './App.css';
       searchTodo = {searchTodo}
       completedTodo = {completedTodo}
       deleteTodo = {deleteTodo}*/
+function App(){
+  const [state, setState] = React.useState('estado compartido');
+  return (
+    <React.Fragment>
+      <TodoHeader>
+        <TodoCounter></TodoCounter>
+        <TodoSearch></TodoSearch>
+      </TodoHeader>
+      <TodoList>
+        <TodoItem state={state}/>
+      </TodoList>
+    </React.Fragment>
+  );
+}
+function TodoHeader({children}){
+  return (
+    <header>{children}</header>
+  );
+}
+function TodoList({children}){
+  return (
+    <section className='mt-4 p-5 bg-primary text-white rounded'>
+      {children}
+    </section>
+  );
+}
+function TodoCounter(){
+  return (
+    <p>TodoCounter</p>
+  );
+}
+function TodoSearch(){
+  return (
+    <p>TodoSearch</p>
+  );
+}
+function TodoItem({state}){
+  return (
+    <p>TodoItem {state}</p>
+  );
+}
 
+      /*
 function App() {
 
   return (
@@ -29,5 +75,5 @@ function App() {
     
   );
 }
-
+*/
 export default App;
